0017eba9de66e12b294db432e51ed3c8a322c99c
aisyahalfina <aisyahalfina04@gmail.com>
2026-09-08 01:36:14 +0700

    Update production API URL

--- client/script.js
+++ client/script.js
@@ -12,7 +12,7 @@ if (savedHistory) {
     conversationHistory = JSON.parse(savedHistory);
 }
 
-const API_URL = "https://server-4f38wwd46-aisyahalfinaas-projects.vercel.app/api/chat";
+const API_URL = "https://server-xi-five-77.vercel.app/api/chat";
 
 
 function addMessage(message, sender) {
