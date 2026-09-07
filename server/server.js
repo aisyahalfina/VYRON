306a2b9ebb0d3ef8f5eefd10d7186a7780bd5a19
aisyahalfina <aisyahalfina04@gmail.com>
2026-09-08 01:31:19 +0700

    Fix CORS preflight

--- server/server.js
+++ server/server.js
@@ -6,17 +6,11 @@ const { GoogleGenAI } = require("@google/genai");
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(204);
-    }
-
-    next();
-});
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"]
+}));
 
 app.use(express.json());
 
